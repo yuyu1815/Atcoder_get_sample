be9976d63d99ea45f648c1d3529da55bbe1461ce
yuzumican <83267351+yuyu1815@users.noreply.github.com>
2024-10-16 22:08:10 +0900

    改変途中

--- src2/gui.py
+++ src2/gui.py
@@ -1,11 +1,15 @@
 import flet as ft
-
+import re,time,json
 from activetab import start_flask
+from recest import contests
 
 
 class GUI:
     def __init__(self):
         self.url_text_field = None
+        self.contest_name = None
+        self.contest_Q_ids = []
+        self.contest_Q_names = []
 
     def start(self, page: ft.Page):
         # error閉じる
@@ -18,6 +22,15 @@ class GUI:
             dlg.open = True
             page.update()
 
+        def route_change(handler):
+            troute = ft.TemplateRoute(handler.route)
+            if troute.match("/view1"):
+                page.views.append(main_window())
+            elif troute.match("/view2"):
+                page.views.append(sab_page())
+            elif troute.match("/view3"):
+                page.views.append(contest_window())
+            page.update()
         # ダイアログの定義
         dlg = ft.AlertDialog(
             title=ft.Text("エラー"),
@@ -39,15 +52,22 @@ class GUI:
                     text="▶",
                     width=100,
                     height=100,
-                    on_click=lambda _: page.go("/view2") if "https://atcoder.jp/contests/a" in self.url_text_field.value else open_dlg("URLが正しくありません")
+                    on_click=lambda _: page.go("/view3") if "https://atcoder.jp/contests/" in self.url_text_field.value else page.go("/view2") if self.url_text_field.value is None else open_dlg("URLが正しくありません")
                 ),
             ])
-
         def sab_page():
             return ft.View("/view2", [
                 ft.AppBar(title=ft.Text("コンテスト情報"),
                           bgcolor=ft.colors.RED),
                 ft.Text("コンテスト情報が読み込み中...")
+            ])
+        def contest_window():
+            self.contest_name = re.search(r"https://atcoder\.jp/contests/([^/]+)", self.url_text_field.value).group(1)
+            self.contest_Q_ids, self.contest_Q_names = contests(self.contest_name)
+            return ft.View("/view3", [
+                ft.AppBar(title=ft.Text("コンテスト"),
+                          bgcolor=ft.colors.RED),
+                ft.Text(f"{self.contest_Q_ids}, {self.contest_Q_names}")
             ])
         page.title = "Atcoder_sample_get"
         page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
@@ -55,10 +75,10 @@ class GUI:
         page.window_width = 500  # 幅
         page.window_height = 650  # 高さ
         # ページにビューとダイアログを追加
-        #page.views.append(sab_page())
+        page.on_route_change = route_change
         page.views.append(main_window())
+        page.views.append(sab_page())
         page.overlay.append(dlg)
-        page.update()
         page.go("/view1")
 
 
